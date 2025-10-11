15dad8831c417640b93771fe606c95a22b549bcb
Anuroopgowda <anuroopgowdac@gmail.com>
2025-10-11 19:24:26 +0530

    Test function2 with restrictions that pr need to approved

--- app.py
+++ app.py
@@ -6,3 +6,7 @@ def main():
 def test_func1():
     '''Test function1 with no conflicts'''
     pass
+
+def test_func2():
+    '''Test function2 with restrictions that pr need to approved'''
+    pass
