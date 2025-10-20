b62c1e0d1de0b28038014c202c5f4291d8bc92b0
441461anu <anuroopgowdac612@gmail.com>
2025-10-20 19:55:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,9 +28,15 @@ def test_func5():
     '''test5 changed'''
     pass
 
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
 
 
 
 def fibonacci(n):
     pass
+
 
