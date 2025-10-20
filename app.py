096b4fcefb2fff501e1b76aaf9b564e1bad4d02b
Anuroopgowda <anuroopgowdac@gmail.com>
2025-10-20 19:42:29 +0530

    modified func

--- app.py
+++ app.py
@@ -30,9 +30,6 @@ def test_func5():
 
 
 def factorial_anurroop(n):
-    result = 1
-    for i in range(1, n + 1):
-        result *= i
-    return result
+    print('factorial anurroop {}'.format(n))
 
 
